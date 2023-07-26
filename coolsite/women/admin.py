228941ada597d998a1bfd3321b8597fb48bad9dd
Agahan14 <Imamidinov.agahan06@gmail.com>
2023-07-26 17:33:21 +0600

    created main page

--- coolsite/women/admin.py
+++ coolsite/women/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from women.models import Women
 
-# Register your models here.
+
+class WomenAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'content', 'photo', 'is_published', 'time_create', 'time_update')
+    list_display_links = ('id', 'title')
+    search_fields = ('title', 'content')
+    list_editable = ('is_published', "content")
+    list_filter = ('is_published', 'time_create')
+
+
+admin.site.register(Women, WomenAdmin)
--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from women.models import Women
+
+menu = ["О сайте", "Добавить статью", "Обратная связь", "Войти"]
 
 
 def index(request):
-    return HttpResponse("<h1>Hello world</h1> My name is Agahan")
+    posts = Women.objects.all()
+    data = {'menu': menu, 'title': 'Главная страница', 'posts': posts}
+    return render(request, "index.html", data)
 
 
-# def about(request):
+def about(request):
+    # posts = Women.objects.all()
+    data = {'menu': menu, 'title': 'О сайте'}
+    return render(request, "index.html", data)
